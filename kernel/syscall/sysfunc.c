8d3f0f399760dc64a2370710c9f594c13bd24cea
joeyxzy <10235102432@stu.ecnu.edu.cn>
2024-11-15 18:51:52 +0800

    Lab6框架

--- kernel/syscall/sysfunc.c
+++ kernel/syscall/sysfunc.c
@@ -88,7 +88,7 @@ uint64 sys_munmap()
 // uint64 addr
 // uint32 len
 // 返回 0
-uint64 sys_copyin()
+/* uint64 sys_copyin()
 {
     proc_t* p = myproc();
     uint64 addr;
@@ -132,4 +132,41 @@ uint64 sys_copyinstr()
     printf("get str from user: %s\n", s);
 
     return 0;
+} */
+
+// 打印字符
+// uint64 addr
+uint64 sys_print()
+{
+
+}
+
+// 进程复制
+uint64 sys_fork()
+{
+
+}
+
+// 进程等待
+// uint64 addr  子进程退出时的exit_state需要放到这里 
+uint64 sys_wait()
+{
+
+}
+
+// 进程退出
+// int exit_state
+uint64 sys_exit()
+{
+
+}
+
+extern timer_t sys_timer;
+
+// 进程睡眠一段时间
+// uint32 second 睡眠时间
+// 成功返回0, 失败返回-1
+uint64 sys_sleep()
+{
+
 }
